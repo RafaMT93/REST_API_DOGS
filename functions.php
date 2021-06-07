1426d9cf2e799661103fcbc1bdf1c973bd15c13f
Rafael Tiburcio <rafat93.rdm@outlook.com.br>
2021-06-07 13:26:12 -0300

    added require photo_post and update options for images

--- functions.php
+++ functions.php
@@ -1,10 +1,15 @@
 <?php 
   //Clean Actions
   remove_action('rest_api_init', 'create_initial_rest_routes', 99);
-  //requires
+  //endpoints
   $dirbase = get_template_directory();
   require_once $dirbase . '/endpoints/user_post.php';
   require_once $dirbase . '/endpoints/user_get.php';
+  require_once $dirbase . '/endpoints/photo_post.php';
+  //update options for img size
+  update_option('large_size_w', 1000);
+  update_option('large_size_h', 1000);
+  update_option('large_crop', 1);
   //Change url wp-json
   function change_api(){
     return 'json';
