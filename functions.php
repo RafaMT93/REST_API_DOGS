0616214b715cddc20f0c1f9dda9b9dad6446f414
Rafael Tiburcio <rafat93.rdm@outlook.com.br>
2021-06-07 17:09:38 -0300

    Added require comment_post

--- functions.php
+++ functions.php
@@ -11,6 +11,8 @@ require_once $dirbase . '/endpoints/user_get.php';
 require_once $dirbase . '/endpoints/photo_post.php';
 require_once $dirbase . '/endpoints/photo_delete.php';
 
+require_once $dirbase . '/endpoints/comment_post.php';
+
 //update options for img size
 update_option('large_size_w', 1000);
 update_option('large_size_h', 1000);
