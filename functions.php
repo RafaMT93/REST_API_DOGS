6c04331acf2b1025e11f0722fa568fdd3407e0ae
Rafael Tiburcio <rafat93.rdm@outlook.com.br>
2021-06-07 16:32:10 -0300

    fixed design pattern

--- functions.php
+++ functions.php
@@ -1,24 +1,30 @@
 <?php 
-  //Clean Actions
-  remove_action('rest_api_init', 'create_initial_rest_routes', 99);
-  //endpoints
-  $dirbase = get_template_directory();
-  require_once $dirbase . '/endpoints/user_post.php';
-  require_once $dirbase . '/endpoints/user_get.php';
-  require_once $dirbase . '/endpoints/photo_post.php';
-  //update options for img size
-  update_option('large_size_w', 1000);
-  update_option('large_size_h', 1000);
-  update_option('large_crop', 1);
-  //Change url wp-json
-  function change_api(){
-    return 'json';
-  }
-  add_filter('rest_url_prefix', 'change_api');
+//Clean Actions
+//remove_action('rest_api_init', 'create_initial_rest_routes', 99);
 
-  function expire_token(){
-    return time() + (60 * 60 * 24);
-  }
-  add_action('jwt_auth_expire', 'expire_token');
+//endpoints
+$dirbase = get_template_directory();
+
+require_once $dirbase . '/endpoints/user_post.php';
+require_once $dirbase . '/endpoints/user_get.php';
+
+require_once $dirbase . '/endpoints/photo_post.php';
+require_once $dirbase . '/endpoints/photo_delete.php';
+
+//update options for img size
+update_option('large_size_w', 1000);
+update_option('large_size_h', 1000);
+update_option('large_crop', 1);
+
+//Change url wp-json
+function change_api(){
+  return 'json';
+}
+add_filter('rest_url_prefix', 'change_api');
+
+function expire_token(){
+  return time() + (60 * 60 * 24);
+}
+add_action('jwt_auth_expire', 'expire_token');
 
 ?>
